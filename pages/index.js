5f70701ee9d4f240195c399bd3d5831eecb35806
David Douglas <douglasdavid177@gmail.com>
2022-06-29 01:50:35 -0400

    set up small animation to results section

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import { useState } from "react";
 import styles from "../styles/Home.module.css";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Home() {
   const [showResults, setShowResults] = useState(false);
+  const [decVal, setDecVal] = useState(1);
+  const [denVal, setDenVal] = useState(1);
+  const calculate = (dec, den) => {
+    const fractionsArray = new Array(den + 1);
+    for (let n = 0; n <= den; n++) {
+      fractionsArray[n] = n / den;
+    }
+    console.log("fractions: ");
+    console.log(fractionsArray);
+    // const closest = fractionsArray.reduce(function (prev, curr) {
+    //   return Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev;
+    // });
+    setShowResults(true);
+  };
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -14,35 +29,31 @@ export default function Home() {
         <h4>Welcome to...</h4>
         <h1>...Lucas&apos;s Conversion Tool!</h1>
 
-        {showResults ? <ResultsSection /> : <InputSection />}
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            key={showResults ? "results" : "input"}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            {showResults ? <ResultsSection /> : <InputSection />}
+          </motion.div>
+        </AnimatePresence>
         <div className={styles.buttonHolder}>
           <button
             onClick={() => {
-              calculate();
+              showResults ? setShowResults(false) : calculate(decVal, denVal);
             }}
           >
-            Results
+            {showResults ? "Change Input" : "View Results"}
           </button>
         </div>
       </div>
     </div>
   );
 
-  const calculate = (dec, den) => {
-    const fractionsArray = new Array(den + 1);
-    for (let n = 0; n <= den; num++) {
-      fractionsArray[n] = n / den;
-    }
-    console.log("fractions: ");
-    console.log(fractionsArray);
-    // const closest = fractionsArray.reduce(function (prev, curr) {
-    //   return Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev;
-    // });
-    setShowResults(true);
-  };
   function InputSection() {
-    const [decVal, setDecVal] = useState(1);
-    const [denVal, setDenVal] = useState(1);
     return (
       <div className={styles.inputSection}>
         <h5>
