83c4c52a0a83c32d4e56391a1407ed1524ce11ca
David Douglas <douglasdavid177@gmail.com>
2022-08-19 00:12:46 -0400

    turn off scrolling to top when switching main sections, make drawer larger and font sizes smaller

--- components/aboutsection.jsx
+++ components/aboutsection.jsx
@@ -6,12 +6,12 @@ const AboutSection = ({ setOpenNav }) => {
     <div className={styles.aboutSection}>
       <h4>
         {`
-        This app is a single page web application (SPA), written in JavaScript with the NextJS framework. It uses
+        This app is a single page web application (SPA), written in JavaScript and built with the NextJS framework. It uses
         ReactJS for frontend architecture, controlling state management, data
         binding, events, application lifecycle and routing. CSS modules are used to apply styling, and animations were written using Framer Motion
-        API. The app consists of various mathemaical tools including: `}
+        API. The app consists of various mathemaical tools described below: `}
       </h4>
-      <h4>{`A decimal to fraction converter, that finds a fraction of a given denominator that's closest to or equivent to a given decimal, `}</h4>
+      <h4>{`The decimal to fraction converter finds a fraction (with a given denominator) that's closest to or equivalent to a given decimal. The results page shows the first and second closet fractions, allowing you to see a high/low value pair. The results are color coded-- showing values in yellow if they are below the target value, green if they are eqivalent, or blue if above. `}</h4>
       <h4>{`A unit conversion tool, for calculating equivalent measurements using different physical units,`}</h4>
       <h4>{`And a tip calculator (coming soon), which helps generate and display convenient tip values based on markers such as nearest percentage or nearest dollar.`}</h4>
       <br />
