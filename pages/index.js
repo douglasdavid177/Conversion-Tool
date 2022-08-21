caafd0e361a8c049e370a8a9ea9ee5677fd97de5
David Douglas <douglasdavid177@gmail.com>
2022-08-21 11:10:46 -0400

    adjust animatepresence transition for buttonholder (remove delay and make duration longer)

--- pages/index.js
+++ pages/index.js
@@ -130,7 +130,7 @@ export default function Home() {
                     animate={{
                       translateY: 0,
                       opacity: 1,
-                      transition: { duration: 0.65, delay: 0.6 },
+                      transition: { duration: 1.25 },
                     }}
                     exit={{
                       translateY: -10,
